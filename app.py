fbec1c60137510e1b09b1a3fe1468a9f1fabbefd
kranthi kumar <g.kranthi2507@gmail.com>
2024-01-24 21:13:19 +0530

    create-room

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, join_room, leave_room
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_login import (
+    LoginManager,
+    login_user,
+    login_required,
+    logout_user,
+    current_user,
+)
 from pymongo import errors
-from db import get_user, save_user
+from db import get_user, save_user, save_room, add_room_members
 
 
 app = Flask(__name__)
@@ -10,81 +16,116 @@ socketio = SocketIO(app)
 app.secret_key = "my secret"
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
- 
-@app.route('/')
-def home():
-    return render_template('index.html')
+login_manager.login_view = "login"
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password_input = request.form.get('password')
+        return redirect(url_for("home"))
+    if request.method == "POST":
+        username = request.form.get("username")
+        password_input = request.form.get("password")
         user = get_user(username)
         if user and user.check_password(password_input):
             login_user(user)
-            return redirect(url_for('home', username=username))
+            return redirect(url_for("home", username=username))
         else:
-            message = 'Failed to login'
-            return render_template('login.html', message=message)
+            message = "Failed to login"
+            return render_template("login.html", message=message)
 
-    return render_template('login.html')
+    return render_template("login.html")
+
 
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for("home"))
 
-@app.route("/signup", methods=['GET', 'POST'])
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    message = ''
-    if request.method == 'POST':
-        username = request.form.get('username')
-        email = request.form.get('email')
-        password_input = request.form.get('password')
+        return redirect(url_for("home"))
+    message = ""
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password_input = request.form.get("password")
         try:
-
             save_user(username=username, password=password_input, email=email)
-            return redirect(url_for('login'))
+            return redirect(url_for("login"))
         except errors.DuplicateKeyError:
             message = "User already exists!"
-    return render_template('signup.html', message=message)
+    return render_template("signup.html", message=message)
 
-@app.route('/chat')
+
+@app.route("/chat")
 def chat():
-    username = request.args.get('username')
-    room = request.args.get('room')
-    app.logger.info("{} values {} ".format(request.args['username'], request.args['room']))
+    username = request.args.get("username")
+    room = request.args.get("room")
+    app.logger.info(
+        "{} values {} ".format(request.args["username"], request.args["room"])
+    )
     if username and room:
-        return render_template('chat.html', username=username, room=room)
+        return render_template("chat.html", username=username, room=room)
     else:
-        return redirect(url_for('home'))
+        return redirect(url_for("home"))
 
-@socketio.on('join_room')
+
+@socketio.on("join_room")
 def handle_join_room(data):
-    app.logger.info("{} has joined the room {} ".format(data['username'], data['room']))
-    join_room(data['room'])
-    socketio.emit('join_room_announcement', data=data, room=data['room'])
+    app.logger.info("{} has joined the room {} ".format(data["username"], data["room"]))
+    join_room(data["room"])
+    socketio.emit("join_room_announcement", data=data, room=data["room"])
 
-@socketio.on('leave_room')
+
+@socketio.on("leave_room")
 def handle_leave_room(data):
-    leave_room(data['room'])
-    socketio.emit('leave_room_announcement', data=data, room=data['room'])
+    leave_room(data["room"])
+    socketio.emit("leave_room_announcement", data=data, room=data["room"])
 
-@socketio.on('send_message')
+
+@socketio.on("send_message")
 def handle_send_message_event(data):
-    app.logger.info("{} has sent message to the room {}: {} ".format(data['username'], data['room'], data['message']))
-    socketio.emit('receive_message', data, room=data['room'])
+    app.logger.info(
+        "{} has sent message to the room {}: {} ".format(
+            data["username"], data["room"], data["message"]
+        )
+    )
+    socketio.emit("receive_message", data, room=data["room"])
+
 
 @login_manager.user_loader
 def load_user(username):
     return get_user(username)
 
-if __name__ == '__main__':
+
+@app.route("/create-room/", methods=["GET", "POST"])
+@login_required
+def create_room():
+    message = ''
+    if request.method == "POST":
+        room_name = request.form.get("room_name")
+        usernames = [
+            username.strip() for username in request.form.get("members").split(",")
+        ]
+        if len(room_name) and len(usernames):
+            room_id = save_room(room_name, current_user.username)
+            if current_user.username in usernames:
+                usernames.remove(current_user.username)
+                add_room_members(room_id, room_name, usernames, current_user.username)
+            else:
+                message = "Failed to create room"
+    return render_template("create_room.html", message=message)
+
+
+
+if __name__ == "__main__":
     socketio.run(app, debug=True)
--- db.py
+++ db.py
@@ -1,18 +1,20 @@
 from bson import ObjectId
-from pymongo import MongoClient
+from pymongo import MongoClient, database
+from pymongo.server_api import ServerApi
 from datetime import datetime
-
+from werkzeug.security import generate_password_hash
 
 from user import User
 
-clinet = MongoClient(
+uri = (
     "mongodb+srv://test:test@cluster0.vel6spq.mongodb.net/?retryWrites=true&w=majority"
 )
+client = MongoClient(uri, server_api=ServerApi("1"))
 
-chat_db = clinet.get_database("chatDB")
-users_collection = chat_db.get_collection("users")
-rooms_collection = chat_db.get_collection("rooms")
-room_members_collection = chat_db.get_collection("room_members")
+chat_db = database.Database(client=client, name="chatDB")
+users_collection = chat_db.users
+rooms_collection = chat_db.rooms
+room_members_collection = chat_db.room_members
 
 
 def save_user(username, email, password):
@@ -31,12 +33,12 @@ def save_room(room_name, created_by):
     room_id = rooms_collection.insert_one(
         {"room_name": room_name, "created_by": created_by, "created_at": datetime.now()}
     )
-    add_room_member(room_id, room_name, created_by, created_by, is_admin=True)
+    add_room_member(room_id, room_name, created_by, created_by, is_room_admin=True)
     return room_id
 
 
 def add_room_member(room_id, room_name, username, added_by, is_room_admin=False):
-    rooms_collection.insert_one(
+    room_members_collection.insert_one(
         {
             "_id": {"room_id": ObjectId(room_id), "username": username},
             "room_name": room_name,
@@ -48,7 +50,7 @@ def add_room_member(room_id, room_name, username, added_by, is_room_admin=False)
 
 
 def add_room_members(room_id, room_name, usernames, added_by):
-    rooms_collection.insert_many(
+    room_members_collection.insert_many(
         [
             {
                 "_id": {"room_id": ObjectId(room_id), "username": username},
@@ -64,17 +66,20 @@ def add_room_members(room_id, room_name, usernames, added_by):
 
 def get_room(room_id):
     # ObjectId will convert str of room_id to of type '_id'
-    rooms_collection.find_one({"_id": ObjectId(room_id)})
+    return rooms_collection.find_one({"_id": ObjectId(room_id)})
 
 
 def update_room(room_id, room_name):
     rooms_collection.update_one(
         {"_id": ObjectId(room_id)}, {"$set": {"name": room_name}}
     )
+    room_members_collection.update_many(
+        {"_id.room_id": ObjectId(room_id)}, {"$set": {"room_name": room_name}}
+    )
 
 
 def get_room_members(room_id):
-    room_members_collection.find({"_id.room_id": ObjectId(room_id)})
+    return list(room_members_collection.find({"_id.room_id": ObjectId(room_id)}))
 
 
 def remove_room_members(room_id, usernames):
@@ -90,16 +95,16 @@ def remove_room_members(room_id, usernames):
 
 
 def get_rooms_for_user(username):
-    room_members_collection.find({"_id.username": ObjectId(username)})
+    return list(room_members_collection.find({"_id.username": ObjectId(username)}))
 
 
 def is_room_member(room_id, username):
-    room_members_collection.count_documents(
+    return room_members_collection.count_documents(
         {"_id": {"room_id": room_id, "username": username}}
     )
 
 
 def is_room_admin(room_id, username):
-    room_members_collection.count_documents(
+    return room_members_collection.count_documents(
         {"_id": {"room_id": room_id, "username": username}, "is_room_admin": True}
     )
